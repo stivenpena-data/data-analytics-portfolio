409c98bd7f922fe1da76dbbb7eb7fed23728d8da
Stiven-data-analytics <jsps2409data@gmail.com>
2026-03-06 14:56:51 -0500

    Update sales_analysis_queries.sql

--- sales-analysis/sql_queries/sales_analysis_queries.sql
+++ sales-analysis/sql_queries/sales_analysis_queries.sql
@@ -23,8 +23,8 @@ ORDER BY total_sales DESC;
 
 -- Monthly Sales Trend
 SELECT
-    MONTH(Order_Date) AS month,
+    MONTH(Date) AS month,
     SUM(Sales) AS monthly_sales
 FROM superstore_sales
-GROUP BY MONTH(Order_Date)
+GROUP BY MONTH(Date)
 ORDER BY month;
