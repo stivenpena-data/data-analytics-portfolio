3d63856cd0b6d85dcb5a471e97ba6fca38a2ca29
Stiven-data-analytics <jsps2409data@gmail.com>
2026-03-05 21:52:14 -0500

    Update sales_analysis_queries.sql

--- sales-analysis/sql_queries/sales_analysis_queries.sql
+++ sales-analysis/sql_queries/sales_analysis_queries.sql
@@ -1,1 +1,30 @@
+-- Sales Analysis Queries
 
+-- Total Revenue
+SELECT
+    SUM(Sales) AS total_revenue
+FROM superstore_sales;
+
+-- Sales by Region
+SELECT
+    Region,
+    SUM(Sales) AS total_sales
+FROM superstore_sales
+GROUP BY Region
+ORDER BY total_sales DESC;
+
+-- Sales by Category
+SELECT
+    Category,
+    SUM(Sales) AS total_sales
+FROM superstore_sales
+GROUP BY Category
+ORDER BY total_sales DESC;
+
+-- Monthly Sales Trend
+SELECT
+    MONTH(Order_Date) AS month,
+    SUM(Sales) AS monthly_sales
+FROM superstore_sales
+GROUP BY MONTH(Order_Date)
+ORDER BY month;
